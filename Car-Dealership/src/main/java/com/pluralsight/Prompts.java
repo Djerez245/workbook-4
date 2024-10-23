a818e8f15772f9b2e82f9ca68fb5d8e5d8d27cac
Damian <jerezdamian640@gmail.com>
2024-10-23 16:15:20 -0400

    added Prompts

--- Car-Dealership/src/main/java/com/pluralsight/Prompts.java
+++ Car-Dealership/src/main/java/com/pluralsight/Prompts.java
@@ -1,4 +1,11 @@
 package com.pluralsight;
 
 public class Prompts {
+
+    public static void printPrompt(String p){
+        System.out.println(p);
+    }
+
+    public static String makeSearch = "What is the make of the vehicle would you like to search for?";
+
 }
