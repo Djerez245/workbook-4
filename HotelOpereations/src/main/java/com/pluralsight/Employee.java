8ed01ef73cff177fd3a70dc121953674d30ca43d
Damian <jerezdamian640@gmail.com>
2024-10-23 16:16:17 -0400

    update

--- HotelOpereations/src/main/java/com/pluralsight/Employee.java
+++ HotelOpereations/src/main/java/com/pluralsight/Employee.java
@@ -1,11 +1,15 @@
 package com.pluralsight;
 
+import java.time.LocalDateTime;
+
 public class Employee {
     private int employeeID;
     private String name;
     private String department;
     private int hoursWorked;
     private double payRate;
+
+    LocalDateTime dateTime = LocalDateTime.now();
 
     public Employee(int employeeID, String name, String department, int hoursWorked, double payRate) {
         this.employeeID = employeeID;
@@ -41,6 +45,12 @@ public class Employee {
     public double getOverTime() {
         return hoursWorked - 40;
     }
+
+    public void punchIn(Employee employee){
+        
+    }
+
+
 }
 
 
