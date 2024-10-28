56b3c983c091eb0fd16ac264856cb51c30189cee
Damian <jerezdamian640@gmail.com>
2024-10-28 12:44:33 -0400

    added input mismatch method to clean code up

--- Car-Dealership/src/main/java/com/pluralsight/Dealership.java
+++ Car-Dealership/src/main/java/com/pluralsight/Dealership.java
@@ -35,93 +35,124 @@ public class Dealership {
     // method to make a feature to search by the make
     public ArrayList<Vehicle> getVehicleByMake(String make) {
         ArrayList<Vehicle> results = new ArrayList<>();
+        boolean inputMatches = false;
         for (Vehicle v : dealershipInventory) {
             if (v.getMake().equalsIgnoreCase(make)) {
+                inputMatches = true;
                 results.add(v);
             }
         }
+       inputMismatch(inputMatches);
         return results;
     }
 
     // method to search by model
     public ArrayList<Vehicle> getVehicleByModel(String model) {
         ArrayList<Vehicle> results = new ArrayList<>();
+        boolean inputMatches = false;
         for (Vehicle v : dealershipInventory) {
             if (v.getModel().equalsIgnoreCase(model)) {
+                inputMatches = true;
                 results.add(v);
             }
         }
+        inputMismatch(inputMatches);
         return results;
     }
 
     //method to search by color
     public ArrayList<Vehicle> getVehicleColor(String color) {
         ArrayList<Vehicle> results = new ArrayList<>();
+        boolean inputMatches = false;
         for (Vehicle v : dealershipInventory) {
             if (v.getColor().equalsIgnoreCase(color)) {
+                inputMatches = true;
                 results.add(v);
             }
         }
+        inputMismatch(inputMatches);
         return results;
     }
 
     //method to search by VIN
     public ArrayList<Vehicle> getVehicleVin(int vin) {
         ArrayList<Vehicle> results = new ArrayList<>();
+        boolean inputMatches = false;
         for (Vehicle v : dealershipInventory) {
             if (v.getVin() == vin) {
+                inputMatches = true;
                 results.add(v);
             }
         }
+       inputMismatch(inputMatches);
         return results;
     }
 
     // method to search by vehicle type
     public ArrayList<Vehicle> getVehicleType(String type) {
         ArrayList<Vehicle> results = new ArrayList<>();
+        boolean inputMatches = false;
         for (Vehicle v : dealershipInventory) {
             if (v.getVehicleType().equalsIgnoreCase(type)) {
+                inputMatches = true;
                 results.add(v);
             }
         }
+        inputMismatch(inputMatches);
         return results;
     }
 
     //method to search by price
     public ArrayList<Vehicle> getVehiclePrice(double price) {
         ArrayList<Vehicle> results = new ArrayList<>();
+        boolean inputMatches = false;
         for (Vehicle v : dealershipInventory) {
             if (v.getPrice() <= price) {
+                inputMatches = true;
                 results.add(v);
             }
         }
+        inputMismatch(inputMatches);
         return results;
     }
 
     // method to search by mileage
     public ArrayList<Vehicle> getVehicleMileage(int miles) {
         ArrayList<Vehicle> results = new ArrayList<>();
+        boolean inputMatches = false;
         for (Vehicle v : dealershipInventory) {
             if (v.getOdometer() <= miles) {
+                inputMatches = true;
                 results.add(v);
             }
         }
+        inputMismatch(inputMatches);
         return results;
     }
 
     // method to search by year
     public ArrayList<Vehicle> getVehicleYear(int year) {
         ArrayList<Vehicle> results = new ArrayList<>();
+        boolean inputMatches = false;
         for (Vehicle v : dealershipInventory) {
+            inputMatches = true;
             if (v.getYear() == year) {
                 results.add(v);
             }
         }
+        inputMismatch(inputMatches);
         return results;
     }
+
 
     public String toStringForFile() {
         return String.format("%s | %s | %.2s", businessName, address, phoneNumber);
     }
 
+    private void inputMismatch(boolean inputMatches){
+        if (!inputMatches){
+            System.out.println("\nSORRY VEHICLE CAN NOT BE FOUND");
+        }
+
+}
 }
